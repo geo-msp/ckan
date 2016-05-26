8f95a7a28a354308bea5606dfcce6023bd2e7b4a
Nicolas Brisebois-Tétreault <nicolas.brisebois-tetreault@msp.gouv.qc.ca>
2016-05-26 10:46:27 -0400

    Mettre Dosis comme fonte pour certains messages d'erreur

--- ckanext/textview/theme/public/text_view.js
+++ ckanext/textview/theme/public/text_view.js
@@ -2,7 +2,7 @@ ckan.module('text_view', function (jQuery, _) {
   return {
     options: {
       i18n: {
-        error: '<p>Les données ne peuvent être visualisées.<br>Elles sont disponibles pour le téléchargement par le bouton Télécharger ci-haut. <br>Pour tout problème avec ce jeu de données, veuillez  <a href="./../../../../../fr/contact">communiquez avec nous</a></p>'
+        error: '<p style="font-family: \'Dosis\', sans-serif;">Les données ne peuvent être visualisées.<br>Elles sont disponibles pour le téléchargement par le bouton Télécharger ci-haut. <br>Pour tout problème avec ce jeu de données, veuillez  <a href="/fr/contact">communiquez avec nous</a></p>'
       },
       parameters: {
         json: {
@@ -68,7 +68,7 @@ ckan.module('text_view', function (jQuery, _) {
         },
         error: function(jqXHR, textStatus, errorThrown) {
           if (textStatus == 'error' && jqXHR.responseText.length) {
-            self.el.html('<p>Les données ne peuvent être visualisées.<br>Elles sont disponibles pour le téléchargement par le bouton Télécharger ci-haut. <br>Pour tout problème avec ce jeu de données, veuillez  <a href="./../../../../../fr/contact">communiquez avec nous</a></p>');
+            self.el.html('<p style="font-family: \'Dosis\', sans-serif;">Les données ne peuvent être visualisées.<br>Elles sont disponibles pour le téléchargement par le bouton Télécharger ci-haut. <br>Pour tout problème avec ce jeu de données, veuillez  <a href="/fr/contact">communiquez avec nous</a></p>');
           } else {
             self.el.html(self.i18n('error', {text: textStatus, error: errorThrown}));
           }
