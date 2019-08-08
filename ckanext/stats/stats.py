cbf4ef056c6691f490ef5864ab24617e0989b4b2
admgeo1 <admgeo1@msp.gouv.qc.ca>
2019-08-08 10:10:27 -0400

    Utilisation de la variable globale site_url pour ne pas avoir a le changer dans chaque environnement

--- ckanext/stats/stats.py
+++ ckanext/stats/stats.py
@@ -52,7 +52,8 @@ PREFIX_URL_DATASET = '/recherche/fr/dataset/'
 '''
 Paths
 '''
-PATH_DATA = "https://rc.donneesquebec.ca/wp-content/uploads/stats_data/"
+SITE_URL = config.get('ckan.site_url', '')[:-10]
+PATH_DATA = SITE_URL + "/wp-content/uploads/stats_data/"
 LICENSE_VALUE_PATH = PATH_DATA + "license_values.json"
 RECOMMENDED_OPEN_FORMAT_PATH = PATH_DATA + "recommended_open_formats.json"
 SECONDARY_OPEN_FORMAT_PATH = PATH_DATA + "secondary_open_formats.json"
